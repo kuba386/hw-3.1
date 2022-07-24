05560c08cb58411b0850f447ecd216205982b5c9
kuba386 <kubajanatbekov@gmail.com>
2022-07-24 22:42:57 +0600

    Initial Commit

---
+++ HW-3.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-3.1
+//
+//  Created by куба жанат on 24.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
